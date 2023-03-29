7289983f2f1b67c211ced7df6fe09b312934ace9
AlexMagargiu <122966645+AlexMagargiu@users.noreply.github.com>
2023-03-29 21:47:00 +0300

    Update JS and HTML for better readability

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
 function switchDisplay(){
-     if(getValue() != true){
-        return;
-    } 
     let baseDisplay = document.querySelector(".base");
     let thanksDisplay = document.querySelector(".thank-you")
     if (baseDisplay.style.display === "flex"){
@@ -12,39 +9,13 @@ function switchDisplay(){
     }
 }
 
-
-//First idea => Not optimized
-/* function getValue(){
-    const poor = document.getElementById("1");
-    const bad = document.getElementById("2");
-    const decent = document.getElementById("3");
-    const good = document.getElementById("4");
-    const excellent = document.getElementById("5");
-    if(poor.checked){
-        document.querySelector(".get-value").innerHTML = poor.value;
-    } else if(bad.checked){
-        document.querySelector(".get-value").innerHTML = bad.value;
-    } else if(decent.checked){
-        document.querySelector(".get-value").innerHTML = decent.value;
-    } else if(good.checked){
-        document.querySelector(".get-value").innerHTML = good.value;
-    } else if(excellent.checked){
-        document.querySelector(".get-value").innerHTML = excellent.value;
-    } else{
-        alert("You've not chosen a number");
-        return false;
-    }
-    return true;
-} */
-
 function getValue(){
     let number = document.querySelector("input[type=radio]:checked");
-    if(number && number.checked){
+    if(number){
         document.querySelector(".get-value").innerHTML = number.value;
-        return true;
+        switchDisplay();
     } else{
         alert("Please choose a rating");
-        return false;
     }
 }
 
